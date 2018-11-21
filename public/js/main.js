cc6bcc84993c096c12c73b287cd5bc13741da5ce
Anish Yadav <hney1000.ay1@gmail.com>
2018-11-22 04:37:02 +0530

    Add new method made the code a bit clean

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,9 @@
-const synth = window.speechSynthesis;
+
+
+const speechSynthesisSupported = 'speechSynthesis' in window;
+
+	let isPaused = false;
+	let isPlaying = false
 
 const textForm = document.querySelector("form");
 const textInput = document.querySelector("#text-input");
@@ -8,135 +13,115 @@ const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
 const voiceSelect = document.querySelector("#select-voice");
 const body = document.querySelector("body");
-//Browser identifier
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
 
-// Chrome 1+
-var isChrome = !!window.chrome && !!window.chrome.webstore;
+let voices = [];
+var translatedVoice = '';
 
-// Init voices array
+const displayVoice = function (voices) {
 
-
-let voices =[];
-
-const getVoices = () => {
-
-            voices = synth.getVoices();
-
-            voices.forEach(voice => {
-                const option = document.createElement('option');
-                option.textContent = voice.name +'('+voice.lang+')';
+	voices.forEach((voice) => {
+     
+    const option = document.createElement('option');
+      option.textContent = voice.name +'('+voice.lang+')';
                 option.setAttribute('data-lang',voice.lang);
     
                 option.setAttribute('data-name',voice.name);
 
                 voiceSelect.appendChild(option);
-    
-    
-              });
 
+	});
 
-};
-
-if (isFirefox) {
-    getVoices();
-}
-if (isChrome) {
-    if (synth.onvoiceschanged !== undefined) {
-        synth.onvoiceschanged = getVoices;
-    }
 }
 
+const getVoices = () => {
+	const voices  = speechSynthesis.getVoices();
 
-const speak = () => {
+	if (voices.length > 0) {
+		displayVoice(voices);
+	}
+}
+    const translate = function(text,lang) {
+	               const Http = new XMLHttpRequest();
+                   XMLHttpRequest.responseType = "json";
+                   var APIkey = 'trnsl.1.1.20180919T092428Z.e002d2bf6d6203f9.5d700814ead3353b8f2b18ecb32708e79a195d10';
+                   var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='+APIkey+'&text='+text+'&lang=en-'+lang+'&[format=plain]';
+                    console.log(text);
+                        Http.open("GET", url,false);
+                        Http.onreadystatechange=(e)=>{
+                        	 if (Http.readyState == 4 && Http.status == 200) {
+                        	 	   var count = 0;
+                                    var responseBody = Http.responseText;
+                                      var data = JSON.parse(responseBody);
+                                        translatedVoice = data.text[0];
+                                    
+                                 }
 
+                             }
+                             Http.send();
+   }
 
+ function speak(TextToSpeech) {
 
-        if(synth.speaking) {
+    if (TextToSpeech !=='') {
 
-            console.error("Already speaking ....");
-            return;
-        }
-        if(textInput.value !== "") {
-                textForm.style.background = '#141414 url(img/wave.gif)';
-                textForm.style.backgroundRepeat = 'repeat-x';
-                textForm.style.backgroundSize = '100% 100%';
-                var count = 1;
+    textForm.style.background = "#FF7417 url('../img/wave.gif')";
 
-                var translatedVoice;
-
-                const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+		const utterence = new SpeechSynthesisUtterance();
+		
+	            const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
                 console.log('Selected voice is '+selectedVoice);
                 const selectedLang =  voiceSelect.selectedOptions[0].getAttribute('data-lang').substring(0,2);
                 console.log('selected lang is '+selectedLang);
-                           const Http = new XMLHttpRequest();
-                           XMLHttpRequest.responseType = "json";
-                           var APIkey = 'trnsl.1.1.20180919T092428Z.e002d2bf6d6203f9.5d700814ead3353b8f2b18ecb32708e79a195d10';
-                           var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='+APIkey+'&text='+textInput.value+'&lang=en-'+selectedLang+'&[format=plain]';
-                            console.log(textInput.value);
-                        Http.open("GET", url);
-                        Http.send();
-                Http.onreadystatechange=(e)=>{
-                                    var responseBody = Http.responseText;
-                                    translatedVoice = JSON.parse(responseBody).text[0];
-                                     console.log('translated text is '+translatedVoice);
-                               
 
+                // Translation part goes here
+          
+                translate(TextToSpeech,selectedLang);
+            
+                 				                 
+					                utterence.text = translatedVoice;
 
+					                utterence.voice = speechSynthesis
+									.getVoices()
+									.filter(function (voice) {
+										return voice.name === selectedVoice ;
+									})[0];;
+					        utterence.lang = selectedLang;
+					        utterence.rate = rate.value;
+                        utterence.pitch = pitch.value;
 
-                        const speakText = new SpeechSynthesisUtterance(translatedVoice);
-                        //set pitch and rate
-                        speakText.lang = voiceSelect.selectedOptions[0].getAttribute('data-lang');
-                        speakText.rate = rate.value;
-                        speakText.pitch = pitch.value;
-
-                        if(count == 1) {
-                        synth.speak(speakText);
-                        console.log(speakText);
-                        console.log(count);
-                        }
-
-                        speakText.onend = e =>{
-
-                        textForm.style.background = '#141414';
-
-                        console.log("Done Speaking ....");
-                       console.log(count);
-                        count = 1;
-
-                        }
-
-                        speakText.onerror = e => {
-                        console.error("Something is wrong ...");
-                        }
-
-                        voices.forEach(voice => {
-                        if( voice.name === selectedVoice){
-                            speakText.voice = voice;
-                           console.log(voice);
-                            return ;
-                       }
-
-
-                        });
-                        count++;
-                        return;
-                }
-
+					        window.speechSynthesis.speak(utterence);
+			    utterence.onend = e => {
+			    	textForm.style.background = '#FF7417';
+			    }
+                  //}
         }
+    }
 
-};
+	
+
+	if (speechSynthesisSupported) {
+		getVoices();
+
+		// Chrome loads voices asynchronously.
+		window.speechSynthesis.onvoiceschanged = () => {
+			getVoices();
+		};
+	}
+   
+    if (speechSynthesisSupported) {
+
+     textForm.addEventListener('submit',e =>{
+     	e.preventDefault();
+
+    	if (textInput.value.length >0 ) {
+
+    		speak(textInput.value);
+    	} 
+   });
 
 
+     rate.addEventListener('change',e=> rateValue.textContent = rate.value);
+    pitch.addEventListener('change',e=> pitchValue.textContent = pitch.value);
 
-textForm.addEventListener('submit',e =>{
-    e.preventDefault();
-    speak();
-    textInput.blur();
-});
-rate.addEventListener('change',e=> rateValue.textContent = rate.value);
-pitch.addEventListener('change',e=> pitchValue.textContent = pitch.value);
-
-voiceSelect.addEventListener('change',e =>  speak());
-
+    voiceSelect.addEventListener('change',e =>  speak(textInput.value));
+    }
